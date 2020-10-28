17e65cf1ae28b28b7657bddf6464c76d74ace52c
alexZeLoCO <alejandro.rod.lop@gmail.com>
2020-10-28 13:41:21 +0100

    Primer Commit

--- ExisteValorEnVector/src/ExisteValorEnVector.java
+++ ExisteValorEnVector/src/ExisteValorEnVector.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;		//IMPORTAR SCANNER
+/**
+ * @author LOS PROFSORES DE IP
+ *	EL PROGRAMA COMPRUEBA SI UN VALOR INTRODUCIDO POR EL USUARIO PERTENECE O NO A UN VECTOR.
+ *	EL VECTOR CONTIENE LOS 10 PRIMEROS NÚMEROS PARES. SIN EL 0.
+ */
 public class ExisteValorEnVector {
 
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner (System.in);		//CREAR TECLADO
 		int [] vector = new int [10];		//CREAR VECTOR
 		
-		for (int a=0;a<=10;a++) {		//RELLENAR VECTOR
+		for (int a=1;a<10;a++) {		//RELLENAR VECTOR
 			vector[a] = a*2;		//10 PRIMEROS PARES
 		}
 		
