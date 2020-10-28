0f2b9665846f00eafa22e2bb2172871b8548a5a9
alexZeLoCO <alejandro.rod.lop@gmail.com>
2020-10-28 13:39:03 +0100

    Primer Commit

---
+++ ExisteValorEnVector/src/ExisteValorEnVector.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;		//IMPORTAR SCANNER
+public class ExisteValorEnVector {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner (System.in);		//CREAR TECLADO
+		int [] vector = new int [10];		//CREAR VECTOR
+		
+		for (int a=0;a<=10;a++) {		//RELLENAR VECTOR
+			vector[a] = a*2;		//10 PRIMEROS PARES
+		}
+		
+		System.out.print("Introduzca un entero: ");		//SOLICITA VALOR
+		int n = teclado.nextInt();		//VALOR ALMACENADO EN N
+		
+		int i = 0;		//INICIALIZAR I
+		while ((i<vector.length)&&(vector[i]!=n)) {		//MIENTRAS I SEA MENOR QUE 10 Y N DISTINTA DE VALOR DE VECTOR
+			i++;		//SUMAR VALOR A I
+		} 
+		if (i<vector.length) {		//SI I ES MENOR QUE TAMAÑO VECTOR (SE ENCONTRÓ EL VALOR N=VECTOR[I])
+			System.out.print("El valor está en el vector.");		//OUTPUT
+		} else {		//SI NO. (NO SE ECONTRÓ N=VECTOR[I])
+			System.out.print("El valor no está en el vector.");		//OUTPUT
+		}
+	}
+}
+
